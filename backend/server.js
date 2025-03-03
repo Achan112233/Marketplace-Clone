791c96c7943c913e50f30f3af77b310d26f7a2b7
Achan112233 <131224571+Achan112233@users.noreply.github.com>
2025-03-02 22:08:53 -0500

    updated express request

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,10 @@ app.get('/pr', (req, res) => {
 app.listen(5000, () => {
     console.log('Server started at http://localhost:5000');
 });
+
+app.get('/pr', (req, res) => {
+    res.send('Hello World!');
+}   );
+app.get('/pr', (req, res) => {
+    res.send('Hello World!');
+});
