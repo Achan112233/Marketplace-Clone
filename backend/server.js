61adb6e2f984a8c44170613ba026ceb7d6df5244
Achan112233 <131224571+Achan112233@users.noreply.github.com>
2025-03-03 21:41:45 -0500

    comments

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv'; 
 
+dotenv.config(); // Load environment variables from .env file
+//importing express module  
 const app = express();
 
 //home route
