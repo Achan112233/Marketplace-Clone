fd83e1334febbdfdc68fe5dd9ea5eabf848c32e7
Achan112233 <131224571+Achan112233@users.noreply.github.com>
2025-03-03 21:35:33 -0500

    setting up products side

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,22 @@ app.get('/pr', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get("/products", (req, res) => {
+    res.send([
+        {
+            id: 1,
+            name: "Product 1",
+            price: 100
+        },
+        {
+            id: 2,
+            name: "Product 2",
+            price: 200
+        }
+    ]);
+});
+
 app.listen(5000, () => {
     console.log('Server started at http://localhost:5000');
 });
 
-app.get('/pr', (req, res) => {
-    res.send('Hello World!');
-}   );
-app.get('/pr', (req, res) => {
-    res.send('Hello World!');
-});
