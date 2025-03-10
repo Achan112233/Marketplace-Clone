9977bb5cf3824fa8e885177e0aa4e051f4233773
Achan112233 <131224571+Achan112233@users.noreply.github.com>
2025-03-10 05:10:07 -0400

    error handling when loading database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'; 
 
-// Load environment variables from secret file
-dotenv.config(); 
+
 export const connectDB = async () => {
     try{
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
     }catch (error){
-
+        console.log(`Error: ${error.message}`.red.bold);
+        process.exit(1);
+    // 1 means failure, 0 success
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,22 @@
 import express from 'express';
 //importing express module  
+import dotenv from 'dotenv'; 
+import { connectDB } from './config/db.js';
+
+// Load environment variables from secret file
+dotenv.config(); 
 const app = express();
 
-console.log(process.env.MONGO_URI);
-//home route
-app.get('/pr', (req, res) => {
-    res.send('Hello World!');
-});
 
 //products route
 app.get("/products", (req, res) => {
-    res.send([
-        {
-            id: 1,
-            name: "Product 1",
-            price: 100
-        },
-        //object 2
-        {
-            id: 2,
-            name: "Product 2",
-            price: 200
-        },
-        //object 3
-        {
-            id: 3,
-            name: "Product 2",
-            price: 200
-        },
-        {
-            id: 4,
-            name: "Product 2",
-            price: 200
-        }
-    ]);
 });
 
+//importing mongoose module
+console.log(process.env.MONGO_URI);
+
 app.listen(5000, () => {
+    connectDB();
+    //connecting to database
     console.log('Server started at http://localhost:5000');
 });
