f0091cd12403a7a622745abacbd32114c45756c8
Achan112233 <131224571+Achan112233@users.noreply.github.com>
2025-03-12 22:51:18 -0400

    product schema created to represent different products

--- models/products.model.js
+++ models/products.model.js
@@ -1,1 +1,20 @@
 import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+}, {
+    //whenever product is created, it will automatically add createdAt and updatedAt fields
+    //createdAt is the date when the product was created and updatedAt is the date when the product was last updated
+    timestamps: true 
+});
