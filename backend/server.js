f80ab2dbd6c263d9c7bb384956612e3b9bb7187d
Achan112233 <131224571+Achan112233@users.noreply.github.com>
2025-04-06 22:00:44 -0400

    small json updates

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import dotenv from dotenv;//need to figure out loading env
 import { connectDB } from './config/db.js';
-
+import dotenv from 'dotenv'; // Load environment variables from .env file
 import productRoutes from './routes/product.route.js'; //importing the product routes
 
+
 // Load environment variables from secret file
-dotenv.config();
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 5000
@@ -14,6 +14,7 @@ app.use(express.json()); //middleware to parse JSON data from request body
 
 app.use("/api/products", productRoutes);
 
+console.log('PORT:', process.env.PORT);
 app.listen(PORT, () => {
     connectDB();
     //connecting to database
